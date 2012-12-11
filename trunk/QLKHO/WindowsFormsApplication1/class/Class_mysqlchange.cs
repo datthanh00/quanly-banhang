8c3780dd8664e543f1098bb8afd02405050d8924
datthanh39@gmail.com <datthanh39@gmail.com@3df51dce-82b4-a0c3-ab8e-48b4d1d0234c>
2012-12-11 19:37:06 +0000

    - up date

---
+++ trunk/QLKHO/WindowsFormsApplication1/class/Class_mysqlchange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApplication1
+{
+	class Mysqlchange:Provider
+	{
+        public SqlConnection SQLConnection1;
+        public SqlCommand SqlCommand1;
+        public SqlDataReader SqlDataReader1;
+
+        public SqlConnection sqlKetNoi;
+        public SqlCommand sqlcmdLenhThucThi;
+        public SqlDataReader sqldatareader;
+        public SqlDataAdapter DA;
+
+        public SqlCommand SqlCommand(string sqlstring,SqlConnection SqlCnn)
+        {
+            return new SqlCommand(sqlstring, SqlCnn);
+        }
+
+        public SqlDataAdapter SqlDataAdapter(string sqlstring, SqlConnection SqlCnn)
+        {
+            return new SqlDataAdapter(sqlstring, SqlCnn);
+        }
+
+        public SqlDataAdapter SqlDataAdapter(SqlCommand sqlcommand)
+        {
+            return new SqlDataAdapter(sqlcommand);
+        }
+    }
+}
