39e69dfac6fdd353dd760ce3792b51c0c0157f51
datthanh39@gmail.com <datthanh39@gmail.com@3df51dce-82b4-a0c3-ab8e-48b4d1d0234c>
2013-01-04 01:33:29 +0000

    + add new dong goi

---
+++ trunk/QLKHO_SQLSERVER/WindowsFormsApplication1/class_import/import.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApplication1.class_import
+{
+    partial class import
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(import));
+            this.tientrinh = new System.Windows.Forms.ProgressBar();
+            this.luoi = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mo = new System.Windows.Forms.OpenFileDialog();
+            this.cbTable = new DevExpress.XtraEditors.ComboBoxEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.luoi)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cbTable.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tientrinh
+            // 
+            this.tientrinh.Location = new System.Drawing.Point(11, 400);
+            this.tientrinh.Name = "tientrinh";
+            this.tientrinh.Size = new System.Drawing.Size(872, 19);
+            this.tientrinh.TabIndex = 5;
+            // 
+            // luoi
+            // 
+            this.luoi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.luoi.Location = new System.Drawing.Point(11, 64);
+            this.luoi.Name = "luoi";
+            this.luoi.RowTemplate.Height = 23;
+            this.luoi.Size = new System.Drawing.Size(872, 330);
+            this.luoi.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbTable);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(433, 57);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thao Tác";
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(352, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Thoát";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(213, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 30);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Lưu Vào Database";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(5, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 30);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Mở Excel";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mo
+            // 
+            this.mo.FileName = "openFileDialog1";
+            // 
+            // cbTable
+            // 
+            this.cbTable.Location = new System.Drawing.Point(107, 26);
+            this.cbTable.Name = "cbTable";
+            this.cbTable.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbTable.Size = new System.Drawing.Size(100, 20);
+            this.cbTable.TabIndex = 38;
+            // 
+            // import
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 420);
+            this.Controls.Add(this.tientrinh);
+            this.Controls.Add(this.luoi);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "import";
+            this.Text = "import";
+            this.Load += new System.EventHandler(this.import_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.luoi)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cbTable.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar tientrinh;
+        private System.Windows.Forms.DataGridView luoi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog mo;
+        private DevExpress.XtraEditors.ComboBoxEdit cbTable;
+    }
+}
---
+++ trunk/QLKHO_SQLSERVER/WindowsFormsApplication1/class_import/import.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace WindowsFormsApplication1.class_import
+{
+    public partial class import : DevExpress.XtraEditors.XtraForm
+    {
+        public import()
+        {
+            InitializeComponent();
+        }
+         private void import_Load(object sender, EventArgs e)
+        {
+            excell  = new importexcell();
+
+            cbTable.Properties.Items.Add("NHà Cung Cấp");
+            cbTable.Properties.Items.Add("Khách Hàng");
+            cbTable.Properties.Items.Add("Mặt Hàng");
+            button2.Enabled = false;
+
+        }
+        public delegate void _deDongTab();
+        public _deDongTab deDongTab;
+        importexcell excell;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mo.Filter = "(*.xls)|*.xls";
+            DataTable bang = new DataTable();
+            if (mo.ShowDialog() == DialogResult.OK)
+            {
+                if (mo.FileName != "")
+                {
+
+                    excell.mo_excel(mo.FileName);
+                    bang = excell.get_data_from_excell();
+                    luoi.DataSource = bang;
+                    button2.Enabled = true;
+                    excell.style_dgview(luoi);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           DataTable dulieu = new DataTable();
+           Provider pv = new Provider();
+           pv.connect();
+            String lenh;
+            dulieu = (DataTable)(luoi.DataSource);
+            tientrinh.Maximum = dulieu.Rows.Count;
+            tientrinh.Minimum = 0;
+            tientrinh.Step = 1;
+            foreach (System.Data.DataRow duyet_dong in dulieu.Rows)
+            {
+                lenh = nhap_tien_tc(duyet_dong);
+                pv.executeNonQuery(lenh);
+                tientrinh.PerformStep();
+            }
+            MessageBox.Show("Quá trình đưa dữ liệu đã hoàn tất.", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            tientrinh.Value = 0;
+        }
+        private String nhap_tien_tc(System.Data.DataRow cot)
+        {
+            String lenh = "";
+            try
+            {
+                
+                if (cbTable.SelectedIndex == 0)
+                {
+                    lenh = "INSERT INTO [NHACUNGCAP]([MANCC],[MAKV],[TENNCC],[DIACHI],[MASOTHUE],[SOTAIKHOAN],[NGANHANG],[SDT],[EMAIL],[FAX],[WEBSITE],[TINHTRANG]) "
+                    + " VALUES ('" + cot["MANCC"] + "','" + cot["MAKV"] + "',N'" + cot["TENNCC"] + "',N'" + cot["DIACHI"] + "','" + cot["MASOTHUE"] + "','" + cot["SOTAIKHOAN"] + "',N'" + cot["NGANHANG"] + "','" + cot["SDT"] + "','" + cot["EMAIL"] + "','" + cot["FAX"] + "','" + cot["WEBSITE"] + "'," + cot["TINHTRANG"] + ") ";
+                }
+                else if (cbTable.SelectedIndex == 1)
+                {
+                    lenh = "INSERT INTO [KHACHHANG] ([MAKH],[MAKV],[TENKH],[SOTAIKHOAN],[NGANHANG],[MASOTHUE],[DIACHI],[SDT],[FAX],[WEBSITE],[YAHOO],[SKYPE],[TINHTRANG]) "
+                + " VALUES ('" + cot["MAKH"] + "','" + cot["MAKV"] + "',N'" + cot["TENKH"] + "','" + cot["SOTAIKHOAN"] + "',N'" + cot["NGANHANG"] + "','" + cot["MASOTHUE"] + "',N'" + cot["DIACHI"] + "','" + cot["SDT"] + "','" + cot["FAX"] + "','" + cot["WEBSITE"] + "','" + cot["YAHOO"] + "','" + cot["SKYPE"] + "'," + cot["TINHTRANG"] + ") ";
+
+                }
+                else if (cbTable.SelectedIndex == 2)
+                {
+                    lenh = "INSERT INTO [MATHANG]([MAMH],[MATH],[MANH],[MAKHO],[TENMH],[MADVT],[SOLUONGMH],[HANSUDUNG],[GIAMUA],[GIABAN],[MOTA],[TINHTRANG]) "
+                    + " VALUES ('" + cot["MAMH"] + "','" + cot["MATH"] + "','" + cot["MANH"] + "','" + cot["MAKHO"] + "',N'" + cot["TENMH"] + "','" + cot["MADVT"] + "'," + cot["SOLUONGMH"] + ",'" + cot["HANSUDUNG"] + "'," + cot["GIAMUA"] + "," + cot["GIABAN"] + ",N'" + cot["MOTA"] + "'," + cot["TINHTRANG"] + ")";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("CỘT TRONG FILE EXCELL KHÔNG HỢP LỆ");
+            }
+            return lenh;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            deDongTab();
+        }
+
+       
+    }
+}
