41b54cfafdc43391c80746c1ca29bda6b6580210
datthanh39 <datthanh39@gmail.com>
2013-03-12 09:47:24 +0000

    + add v1.1.0

---
+++ QLKHO_SQLSERVER/WindowsFormsApplication1/Load/frmActive.designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmActive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTenTaiKhoan = new DevExpress.XtraEditors.LabelControl();
+            this.txtcode = new DevExpress.XtraEditors.TextEdit();
+            this.lbDangNhap = new DevExpress.XtraEditors.LabelControl();
+            this.btDangNhap = new DevExpress.XtraEditors.SimpleButton();
+            this.btKetThuc = new DevExpress.XtraEditors.SimpleButton();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dxErrorProvider1 = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.txtcode.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTenTaiKhoan
+            // 
+            this.lbTenTaiKhoan.Location = new System.Drawing.Point(108, 52);
+            this.lbTenTaiKhoan.Name = "lbTenTaiKhoan";
+            this.lbTenTaiKhoan.Size = new System.Drawing.Size(58, 13);
+            this.lbTenTaiKhoan.TabIndex = 1;
+            this.lbTenTaiKhoan.Text = "Code Active";
+            // 
+            // txtcode
+            // 
+            this.txtcode.Location = new System.Drawing.Point(178, 49);
+            this.txtcode.Name = "txtcode";
+            this.txtcode.Size = new System.Drawing.Size(198, 20);
+            this.txtcode.TabIndex = 0;
+            // 
+            // lbDangNhap
+            // 
+            this.lbDangNhap.Appearance.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDangNhap.Appearance.Options.UseFont = true;
+            this.lbDangNhap.Location = new System.Drawing.Point(129, 12);
+            this.lbDangNhap.Name = "lbDangNhap";
+            this.lbDangNhap.Size = new System.Drawing.Size(247, 22);
+            this.lbDangNhap.TabIndex = 1;
+            this.lbDangNhap.Text = "Nhập Mã Kích Hoạt Phần Mềm";
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.Location = new System.Drawing.Point(178, 86);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(101, 23);
+            this.btDangNhap.TabIndex = 3;
+            this.btDangNhap.Text = "Kích Hoạt";
+            this.btDangNhap.Click += new System.EventHandler(this.btDangNhap_Click);
+            // 
+            // btKetThuc
+            // 
+            this.btKetThuc.Location = new System.Drawing.Point(285, 86);
+            this.btKetThuc.Name = "btKetThuc";
+            this.btKetThuc.Size = new System.Drawing.Size(91, 23);
+            this.btKetThuc.TabIndex = 4;
+            this.btKetThuc.Text = "Kết Thúc";
+            this.btKetThuc.Click += new System.EventHandler(this.btKetThuc_Click);
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Xmas 2008 Blue";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.security;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dxErrorProvider1
+            // 
+            this.dxErrorProvider1.ContainerControl = this;
+            // 
+            // frmActive
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 116);
+            this.ControlBox = false;
+            this.Controls.Add(this.btKetThuc);
+            this.Controls.Add(this.btDangNhap);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtcode);
+            this.Controls.Add(this.lbDangNhap);
+            this.Controls.Add(this.lbTenTaiKhoan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmActive";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kích Hoạt Tài Khoản";
+            ((System.ComponentModel.ISupportInitialize)(this.txtcode.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lbTenTaiKhoan;
+        private DevExpress.XtraEditors.TextEdit txtcode;
+        private DevExpress.XtraEditors.LabelControl lbDangNhap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevExpress.XtraEditors.SimpleButton btDangNhap;
+        private DevExpress.XtraEditors.SimpleButton btKetThuc;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider dxErrorProvider1;
+    }
+}
