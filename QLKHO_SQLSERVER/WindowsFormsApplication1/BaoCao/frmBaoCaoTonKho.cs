88938cd738f9d08ebfcac10809ab834abde8885d
datthanh39 <datthanh39@gmail.com>
2013-04-01 11:18:38 +0000

    - update V1.1.2

--- QLKHO_SQLSERVER/WindowsFormsApplication1/BaoCao/frmBaoCaoTonKho.cs
+++ QLKHO_SQLSERVER/WindowsFormsApplication1/BaoCao/frmBaoCaoTonKho.cs
@@ -173,11 +173,11 @@ namespace WindowsFormsApplication1
             NGAYKT = NGAYKT.Substring(6, 4) + "/" + NGAYKT.Substring(3, 2) + "/" + NGAYKT.Substring(0, 2);
             dto.NGAYKTKHO = NGAYKT;
 
-            int ingaybd =Convert.ToInt32(dateTu.Text.Substring(6, 4)) + Convert.ToInt32(dateTu.Text.Substring(3, 2))*31 + Convert.ToInt32(dateTu.Text.Substring(0, 2))*365;
-            int ingaykt = Convert.ToInt32(dateDen.Text.Substring(6, 4))  + Convert.ToInt32(dateDen.Text.Substring(3, 2))*31  + Convert.ToInt32(dateDen.Text.Substring(0, 2))*365;
+            int ingaybd = Convert.ToInt32(dateTu.Text.Substring(6, 4)) * 365 + Convert.ToInt32(dateTu.Text.Substring(3, 2)) * 31 + Convert.ToInt32(dateTu.Text.Substring(0, 2));
+            int ingaykt = Convert.ToInt32(dateDen.Text.Substring(6, 4)) * 365 + Convert.ToInt32(dateDen.Text.Substring(3, 2)) * 31 + Convert.ToInt32(dateDen.Text.Substring(0, 2));
             if (ingaybd>ingaykt)
             {
-                MessageBox.Show("ngày kết thúc phải nhỏ hơn ngày bắt đầu");
+                MessageBox.Show("ngày kết thúc phải lớn hơn ngày bắt đầu");
                 return;
             }
                 
