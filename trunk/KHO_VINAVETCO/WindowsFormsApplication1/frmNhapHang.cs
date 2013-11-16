46aa59d44742cd039c777941d9c44f74d3166a86
datthanh39@gmail.com <datthanh39@gmail.com@3df51dce-82b4-a0c3-ab8e-48b4d1d0234c>
2013-11-16 11:38:04 +0000

    - update

--- trunk/KHO_VINAVETCO/WindowsFormsApplication1/frmNhapHang.cs
+++ trunk/KHO_VINAVETCO/WindowsFormsApplication1/frmNhapHang.cs
@@ -1569,9 +1569,17 @@ namespace WindowsFormsApplication1
             Int64 _cktien = Convert.ToInt64(DT.Rows[0]["CKTIEN"].ToString());
             cktien.Value = _cktien;
             Int64 thanhtien = tienchuack;
-            if (_cktien > 0 && thanhtien > 0)
+            if (_cktien >= 0 && thanhtien >= 0)
             {
-                ckphantram.Value = Convert.ToDecimal(_cktien / thanhtien * 100);
+                if (thanhtien > 0)
+                {
+                    ckphantram.Value = Convert.ToDecimal(_cktien / thanhtien * 100);
+                }
+                else
+                {
+                    ckphantram.Value = 0;
+                    cktien.Value = 0;
+                }
             }
             else
             {
@@ -1826,7 +1834,16 @@ namespace WindowsFormsApplication1
             Int64 _cktien = Convert.ToInt64(cktien.Value);
             if (_cktien >= 0 && thanhtien >= 0)
             {
-                ckphantram.Value = Convert.ToDecimal(_cktien / thanhtien * 100);
+                if (thanhtien > 0)
+                {
+                    ckphantram.Value = Convert.ToDecimal(_cktien / thanhtien * 100);
+                }
+                else
+                {
+                    ckphantram.Value = 0;
+                    cktien.Value = 0;
+                }
+               
             }
             else
             {
