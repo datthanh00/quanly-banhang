3a84857fc2521a94b6fb193439823c6af50642f5
datthanh00 <datthanh39@gmail.com>
2015-12-24 11:57:30 +0700

    - update

---
+++ KHO_VINAVETCO/WindowsFormsApplication1/class/FormatDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+	class FormatDate
+	{
+        public static void Main()
+        {
+            
+        System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("en-GB");
+        cultureInfo.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy";
+        cultureInfo.DateTimeFormat.LongTimePattern = "dd/MM/yyyy hh:mm:ss";
+        System.Windows.Forms.Application.CurrentCulture = cultureInfo;
+        System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("en-GB");
+
+        }
+	}
+}
